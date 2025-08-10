2517f6279f15c98068ba85937afc04947baeefbc
Koushik <banerjeekoushik04@gmail.com>
2025-08-10 06:41:33 +0530

    adding chnages in code

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,38 @@
-import { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
-import authService from "./appwrite/auth";
-import { login, logout } from "./store/authSlice";
-import "./App.css";
-import Header from "./components/header/Header.jsx";
-import Footer from "./components/footer/Footer.jsx";
-import { Outlet } from "react-router-dom";
-import { FadeLoader } from "react-spinners";
+import React, { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import './App.css'
+import authService from "./appwrite/auth"
+import {login, logout} from "./store/authSlice"
+import { Footer, Header } from './components'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
   useEffect(() => {
-    authService
-      .getCurrentUser()
-      .then((userData) => {
-        if (userData) {
-          dispatch(login(userData));
-        } else {
-          dispatch(logout());
-        }
-      })
-      // .catch((error) => {
-      //   console.error("Error fetching user:", error);
-      //   dispatch(logout());
-      // })
-      .finally(() => setLoading(false))
-  }, [dispatch]);
-
-  if (loading) {
-    return (
-      <div className="min-h-screen flex items-center justify-center bg-gray-400 text-black">
-        <FadeLoader />
-      </div>
-    );
-  }
-
-  return (
-    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
-      <div className="w-full block">
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
+  
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
         <Header />
         <main>
-          Todo: <Outlet/>
+        TODO:  <Outlet />
         </main>
         <Footer />
       </div>
     </div>
-  );
+  ) : null
 }
 
-export default App;
+export default App
--- src/components/header/LogoutButton.jsx
+++ src/components/header/LogoutButton.jsx
@@ -13,7 +13,7 @@ export default function LogoutButton() {
       .catch((error) => console.log("logoutHandler :: error", error));
   };
   return (
-    <button className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full">
+    <button className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full" onClick={logoutHandler}>
       logout
     </button>
   );
